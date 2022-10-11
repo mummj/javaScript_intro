2b6a07225d15017d21a9240b661acb78bc7a4d6e
mummj <mummj@eou.edu>
2022-10-11 14:34:01 -0700

    intro to js

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("hello world");
+
+let animal = 'RAT!';
+
+console.log(animal);
+
+var number1 = 5;
+
+var number2 = 10;
+
+function mathIntro(number1, number2){
+    console.log("I have two numbers " + number1 + " and " + number2 + ". Lets do some math with these numbers!")
+}
+
+let sum = number1 + number2;
+let diff = number1 - number2;
+let mult = number1 * number2;
+let div = number2 / number1;
+
+mathIntro(2, 5);
